82f67bc9a2901b9f1767dbbd3d12928c176c3610
devmashum <dev.mashum@gmail.com>
2023-09-08 12:50:28 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todo from './todo'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -17,11 +18,33 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <Todo task='Learn React'
+        isDone={true}></Todo>
+      <Todo
+        task='Explore core concept'
+        isDone={false}></Todo>
+      <Todo
+        task='Try JSX'
+        isDone={true}></Todo>
+
+      {/* <Device name='Laptop' price='55'></Device>
+      <Device name='Mobile' price='30000'></Device>
+      <Device name='Watch' price='300'></Device>
       <Person></Person>
+      <Student garde='7' score='99'></Student>
+      <Student garde='10' score='80'></Student>
+      <Student garde='9' score='70'></Student>
+      <Student garde='5' score='100'></Student>
       <Student></Student>
       <Developer></Developer>
+      <Third></Third> */}
     </>
   )
+  function Device(props) {
+    return <h2>This device{props.name} {props.price}</h2>
+  }
+
+
 
   function Person() {
 
@@ -32,21 +55,48 @@ function App() {
     return <h3>I am {person.name}. I am {person.age} years old</h3>
 
   }
-  function Student() {
+  // destructuring Method
+
+  const { garde, score } = { garde: '5', score: '100' }
+
+  function Student({ garde = 1, score = 0 }) {
     return (
       <div className='student'>
-        <h3>This is a student</h3>
-        <p>Name:</p>
-        <p>age;</p>
+        <h3>This is a student. </h3>
+        <p>Grade: {garde}</p>
+        <p>Score: {score}</p>
       </div>
     )
   }
   function Developer() {
+    const developerStyle = {
+      border: '2px solid orange',
+      padding: '20px',
+      margin: '20px',
+      borderRadius: '20px',
+
+    }
     return (
-      <div>
+      <div style={developerStyle}>
         <h5>Devoloper </h5>
         <p>coading</p>
       </div>
+    )
+  }
+
+  function Third() {
+
+    return (
+      <div style={{
+        border: '2px solid orange',
+        padding: '20px',
+        margin: '20px',
+        borderRadius: '20px',
+      }}>
+        <h5>Devoloper new way </h5>
+        <p>coading</p>
+        <p>new style</p>
+      </div >
     )
   }
 
---
+++ src/Todo.jsx
@@ -0,0 +1,54 @@
+// export default function Todo({ task, isDone }) {
+
+//     return (
+//         <li>Task: {task, isDone}</li>
+//     )
+// }
+
+// Conditional rendering option:1
+
+// export default function Todo({ task, isDone }) {
+
+//     if (isDone === true) {
+//         return <li>Finished:{task}</li>
+
+//     } else {
+//         return <li>Work on:{task}</li>
+//     }
+// }
+
+// Conditional rendering option:2
+
+// export default function Todo({ task, isDone }) {
+
+//     if (isDone === true) {
+//         return <li>Finished:{task}</li>
+
+//     }
+//     return <li>Work on:{task}</li>
+
+// }
+
+// Conditional rendering option:3 TERNARY Operator..
+
+
+// export default function Todo({ task, isDone }) {
+
+//     return (
+//         <li> {isDone ? 'Done' : 'Work On'}: {task}</ li>
+
+//     )
+
+// }
+
+// Conditional rendering option:4 &&
+
+
+export default function Todo({ task, isDone }) {
+
+    return (
+        <li> {isDone && 'DONE' || 'NOTE DONE'}: {task}</ li>
+
+    )
+
+}
